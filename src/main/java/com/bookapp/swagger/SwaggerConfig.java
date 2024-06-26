961afbd25c35dfedd215ee1a68b70ec417f5827f
Aditya kumar <ak88564@gmail.com>
2024-06-27 00:35:05 +0530

    Swagger link added

--- src/main/java/com/bookapp/swagger/SwaggerConfig.java
+++ src/main/java/com/bookapp/swagger/SwaggerConfig.java
@@ -11,6 +11,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+/*
+http://localhost:8080/swagger-ui.html
+ */
 @Component
 @EnableSwagger2
 public class SwaggerConfig {
